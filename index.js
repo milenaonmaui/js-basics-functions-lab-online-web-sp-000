27bd7e241a8444d541057ca14ca5ce4cd0531be6
Milena Kari <milenakari@yahoo.com>
2019-08-15 00:33:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function distanceTravelledInFeet(arg1, arg2) {
 }
 
 function calculatesFarePrice(start, destination) {
-  feet = Math.abs(start-destination)
+  feet = distanceTravelledInFeet(start, destination)
   if (feet <= 400) {
     return 0;
   } else if (feet < 2000) {
-    return feet*2
+    return (feet-400)*2/100
+  } else {
+    
   }
-  }
-}
 }
